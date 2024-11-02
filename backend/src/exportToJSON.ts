e6de6c97d15dbc03d2ec2b5ebea3d7339388f21c
Maxim Lutsan <maxim@lutsan.name>
2024-11-02 12:40:04 +0100

    Add array export support

--- backend/src/exportToJSON.ts
+++ backend/src/exportToJSON.ts
@@ -27,22 +27,32 @@ function arrayToJson(array: any[][], startingColumnNumber: number): any[] | null
         let rowHasData = false;
         for (let i = 0; i < keys.length; i++) {
             const keyParts = keys[i].split('.');
-
             let currentObj = obj;
             for (let j = 0; j < keyParts.length; j++) {
                 let part = keyParts[j];
+
+                let isArray = false;
+
+                if (j + 1 < keyParts.length) {
+                    const nextLevel = keyParts[j + 1];
+                    isArray = !isNaN(parseInt(nextLevel));
+                }
+
                 if (part == "") {
                     const currentColumn = i + startingColumnNumber;
                     const columnName = columnToLetter(currentColumn);
                     part = "UNNAMED_" + columnName;
                 }
-                
                 if (j === keyParts.length - 1) {
                     currentObj[part] = valueRow[i];
                     rowHasData = rowHasData || (valueRow[i] !== "");
                 } else {
                     if (!currentObj[part] || typeof currentObj[part] !== 'object') {
-                        currentObj[part] = {};
+                        if (isArray) {
+                            currentObj[part] = [];
+                        } else {
+                            currentObj[part] = {};
+                        }
                     }
                     currentObj = currentObj[part];
                 }
