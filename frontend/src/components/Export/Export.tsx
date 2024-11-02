fb37a84f0d050a8f39eaa421ac395954494a0fec
George <minkogeorge@gmail.com>
2024-11-02 18:27:23 +0100

    Improve export styles

--- frontend/src/components/Export/Export.tsx
+++ frontend/src/components/Export/Export.tsx
@@ -3,77 +3,88 @@ import { Button } from "../../framework/Button/Button";
 
 import { FC, useCallback, useState } from "react";
 import styled from "styled-components";
+import { Loader } from "../../framework/Loader/Loader";
 
 
 export const Export: FC = () => {
-    const [isDownloading, setIsDownloading] = useState(false);
-    const [exportSelectionOnly, setExportSelectionOnly] = useState("false");
+    const [downloading, setDownloading] = useState(false);
+    const [exportFrom, setExportFrom] = useState<"sheet" | "selection">("sheet");
 
     const handleDownload = useCallback(async () => {
         try {
-            await google.script.run.withSuccessHandler((response: { data: string, fileName: string }) => {
+            google.script.run.withSuccessHandler((response: { data: string, fileName: string; }) => {
                 const byteCharacters = atob(response.data);
                 const byteNumbers = new Array(byteCharacters.length);
                 for (let i = 0; i < byteCharacters.length; i++) {
                     byteNumbers[i] = byteCharacters.charCodeAt(i);
                 }
                 const byteArray = new Uint8Array(byteNumbers);
-                const blob = new Blob([byteArray], { type: 'application/json' });
+                const blob = new Blob([byteArray], { type: "application/json" });
 
                 // Create a temporary download link
-                const link = document.createElement('a');
+                const link = document.createElement("a");
                 link.href = URL.createObjectURL(blob);
                 const currentTimestamp = Date.now();
-                link.download = response.fileName + "_" + currentTimestamp + '.json'; // Set the filename
+                link.download = response.fileName + "_" + currentTimestamp + ".json"; // Set the filename
                 link.click(); // Trigger the download
-                setIsDownloading(false);
+                setDownloading(false);
             })
                 .withFailureHandler(() => {
-                    setIsDownloading(false);
-                    alert("Error while generating file")
+                    setDownloading(false);
+                    alert("Error while generating file");
                 })
-                .sheetDataToArray(exportSelectionOnly == "true");
-            setIsDownloading(true);
+                .sheetDataToArray(exportFrom == "selection");
+            setDownloading(true);
         } catch (error) {
-            console.error('Error downloading the file:', error);
+            console.error("Error downloading the file:", error);
         }
-    }, [exportSelectionOnly]);
+    }, [exportFrom]);
 
     return <Container>
         <RadioGroup
-            disabled={isDownloading}
-            description="Export range"
+            disabled={downloading}
+            description="Export from"
             options={[
                 {
-                    value: "false",
+                    value: "sheet",
                     label: "Current sheet"
                 },
                 {
-                    value: "true",
+                    value: "selection",
                     label: "Current selection"
                 }
             ]}
-            defaultValue={exportSelectionOnly}
-            onChange={setExportSelectionOnly}
+            defaultValue={exportFrom}
+            onChange={(value) => setExportFrom(value as "sheet" | "selection")}
         />
-        <Button
-            onClick={handleDownload}
-            disabled={isDownloading}
-        >
-            {isDownloading ? "Please wait..." : "Download data as JSON"}
-        </Button>
+        <ButtonContainer>
+            <Button
+                onClick={handleDownload}
+                disabled={downloading}
+            >
+                Export
+            </Button>
+            {downloading && <Loader />}
+        </ButtonContainer>
     </Container>;
 };
 
 
 const Container = styled.div`
-    position: relative;
     width: 100%;
     height: 100%;
     display: flex;
     flex-flow: column nowrap;
     justify-content: flex-start;
-    gap: 8px;
-    padding: 12px;
+    gap: 16px;
+    padding: 16px;
     padding-top: 0px;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 16px;
 `;
